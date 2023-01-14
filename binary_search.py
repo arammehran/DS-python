71ddacac4fe366e58ecb536d16304a75e899b7d0
Mehran Gul <arammehran123@gmail.com>
2023-01-14 23:00:32 +0530

    (main): error fixed

--- binary_search.py
+++ binary_search.py
@@ -26,5 +26,5 @@ def binary_search(input_array, value):
 test_list = [1,3,9,11,15,19,29]
 test_val1 = 25
 test_val2 = 19
-# print (binary_search(test_list, test_val1))
+print (binary_search(test_list, test_val1))
 print (binary_search(test_list, test_val2))
