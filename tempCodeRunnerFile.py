6e87fd950d90b1310b74728e644c5d62069d0819
Mehran Gul <arammehran123@gmail.com>
2023-01-14 22:57:05 +0530

    (main):binary search done

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,1 @@
-# Test enqueue
-q.enqueue(4)
-# Should be 2
-print (q.dequeue())
-# Should be 3
-print (q.dequeue())
-# Should be 4
-print (q.dequeue())
-q.enqueue(5)
-# Should be 5
-print (q.peek())
+print (binary_search(test_list, test_val1))
