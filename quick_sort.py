c9925c44d4a4098295202b17c4d43b284f6f865c
Mehran Gul <arammehran123@gmail.com>
2023-01-15 20:29:25 +0530

    (main):quick_sort implemented

---
+++ quick_sort.py
@@ -0,0 +1,29 @@
+"""
+Implement quick sort in Python.
+"""
+# Function to find the partition position
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    return i + 1
+
+def quickSort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+
+        # Recursive call on the left of pivot
+        quickSort(array, low, pi - 1)
+
+        # Recursive call on the right of pivot
+        quickSort(array, pi + 1, high)
+
+
+test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
+size = len(test)
+quickSort(test, 0, size - 1)
+print(test)
